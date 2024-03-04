21cb1e44dae9d5b11c8e4be9f577f4f5d900574f
uchanyoung <pbl3_t5>
2024-03-04 22:36:28 +0900

    2

--- src/app/server/db.js
+++ src/app/server/db.js
@@ -2,8 +2,9 @@
 
 const mysql = require('mysql');
 
-// MariaDB 연결 설정
-const connection = mysql.createConnection({
+// MariaDB 연결 풀 생성
+const pool = mysql.createPool({
+    connectionLimit: 10, // 연결 풀의 최대 연결 수
     host: 'svc.sel5.cloudtype.app',
     port: 31081,
     user: 'root',
@@ -11,14 +12,16 @@ const connection = mysql.createConnection({
     database: 'okkyAccessCounts'
 });
 
-
-// 데이터베이스 연결
-connection.connect((err) => {
+// 데이터베이스 연결 풀 사용
+pool.getConnection((err, connection) => {
     if (err) {
         console.error('Error connecting to MariaDB:', err);
         return;
     }
     console.log('Connected to MariaDB');
+
+    // 연결을 반환하여 풀에 다시 반환
+    connection.release();
 });
 
-module.exports = connection;
+module.exports = pool;
--- src/app/server/updateAccessCount/page.js
+++ src/app/server/updateAccessCount/page.js
@@ -3,30 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
+const db = require('../db');
 
 export default function handler(req, res) {
-
-
-
-
-    // MariaDB 연결 설정
-    const connection = mysql.createConnection({
-        host: 'svc.sel5.cloudtype.app',
-        port: 31081,
-        user: 'root',
-        password: 'test',
-        database: 'okkyAccessCounts'
-    });
-
-
-    // 데이터베이스 연결
-    connection.connect((err) => {
-        if (err) {
-            console.error('Error connecting to MariaDB:', err);
-            return;
-        }
-        console.log('Connected to MariaDB');
-    });
 
     // 클라이언트로부터의 요청에서 데이터 추출
     const { countParameter } = req.searchParams; // URL로부터 쿼리 매개변수 추출
@@ -34,7 +13,7 @@ export default function handler(req, res) {
     // 데이터베이스에 count 값과 함께 삽입 또는 갱신하는 쿼리 실행
     const query = `INSERT INTO AccessCounts (count) VALUES (${countParameter})`;
 
-    connection.query(query, (err, result) => {
+    db.query(query, (err, result) => {
         if (err) {
             console.error('Error updating access count:', err);
         } else {
